6ee67591a114d929449ba1ef90b01fcccb446470
andyp <andy.pandaan@example.com>
2025-03-01 23:45:27 +0400

    corrected ansible provisioning

--- vagrant/ansible-playbook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Waiting for all VMs to be fully up..."
-sleep 90
-echo "Running Ansible playbook..."
-ansible-playbook -i ../inventory ../site.yaml --extra-vars 'ansible_ssh_pass=vagrant ansible_become_pass=vagrant'
